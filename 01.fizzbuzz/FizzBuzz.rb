ac689bbaf89b8f141f7f3dd01355f2acfb13f240
Mochihiro <takiwai3001@gmail.com>
2021-05-13 23:05:09 +0900

    レビューを反映

--- 01.fizzbuzz/FizzBuzz.rb
+++ 01.fizzbuzz/FizzBuzz.rb
@@ -1,16 +1,14 @@
 #!/usr/bin/env ruby
 
-number = 0
-while number < 20
-    number += 1
-    case
-    when number % 3 == 0 && number % 5 != 0
-        puts "Fizz"
-    when number % 3 != 0 && number % 5 == 0
-        puts "Buzz"
-    when number % 3 == 0 && number % 5 == 0
-        puts "FizzBuzz"
-    else
-        puts number
-    end
+(1..20).each do |number|
+  if number % 15 == 0
+    puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  else
+    puts number
+  end
 end
+
