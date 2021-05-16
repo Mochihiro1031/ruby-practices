4f177b1e3bd1cf45fcc7aa285e6eb7e7665100ad
Mochihiro <takiwai3001@gmail.com>
2021-05-16 17:09:31 +0900

    レビューを反映

--- 02.calendar/calender_mochihiro.rb
+++ 02.calendar/calender_mochihiro.rb
@@ -3,12 +3,13 @@
 require 'date'
 require 'optparse'
 require 'paint'
-  this_year = Date.today.year
-  this_month = Date.today.month
-  today = Date.today.day
-  params = ARGV.getopts("", "y:#{this_year}", "m:#{this_month}")
-  y = params["y"].to_i
-  m = params["m"].to_i
+
+this_year = Date.today.year
+this_month = Date.today.month
+today = Date.today.day
+params = ARGV.getopts("", "y:#{this_year}", "m:#{this_month}")
+y = params["y"].to_i
+m = params["m"].to_i
 
 if y > 9999 || y < 1
   print("cal: year `#{y}' not in range 1..9999")
@@ -20,14 +21,13 @@ if m > 12 || m < 1
   exit
 end
 
-  month_start = Date.new(y,m,1)
-  month_end = Date.new(y,m,-1)
-  print("      ",m,"月 ",y,"\n")
-  print("日 月 火 水 木 金 土","\n")
-  print("   " * month_start.wday)
+month_start = Date.new(y,m,1)
+month_end = Date.new(y,m,-1)
+puts("      #{m}月 #{y}")
+puts("日 月 火 水 木 金 土")
+print("   " * month_start.wday)
 
-(1..31).each do |day|
-  break if day == month_end.day + 1
+(1..month_end.day).each do |day|
   if [day,y,m] == [today,this_year,this_month]
     print(Paint[today.to_s.rjust(2),:inverse],(" "))
   else
@@ -36,4 +36,4 @@ end
   print("\n") if (month_start.wday + day) % 7 == 0
 end
   print("\n") if (month_start.wday + month_end.day) % 7 != 0
-
+  
